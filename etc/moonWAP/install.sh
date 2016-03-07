92e9ea88ceb278771151e7cc1a129f2b9f91b1d4
bigjosh <github@junk.josh.com>
2016-03-07 15:35:56 -0500

    cleanup

--- etc/moonWAP/install.sh
+++ etc/moonWAP/install.sh
@@ -3,5 +3,5 @@
 /etc/moonWAP/updateMoon.sh
 
 #set us up to autorun every minute
-echo "* * * * * /etc/moonWAP/updatemoon.sh" >>newchrontab.txt
+echo "* * * * * /etc/moonWAP/updatemoon.sh" >newchrontab.txt
 crontab -e
