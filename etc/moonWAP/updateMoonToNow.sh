cc7a95199a92965e37b2d30132f3f421cc70e17d
bigjosh <github@junk.josh.com>
2016-04-13 10:44:40 -0400

    Make dir for calc info page

--- etc/moonWAP/updateMoonToNow.sh
+++ etc/moonWAP/updateMoonToNow.sh
@@ -4,5 +4,16 @@
 # calls updateMoon.sh in same directory
 #  Creates /etc/nodogsplash/htdocs/status.html
 
+# save output to an info page
+infopage="/etc/nodogsplash/htdocs/pages/mooncalc.html"
+
+# prophlactically make the directory
+mkdir $(dirname $infopage)
+
 now=$(date "+%Y-%m-%d %H:%M:%S")
-$(dirname $0)/updateMoon.sh "$now" >/etc/nodogsplash/htdocs/status.html
+
+# save the output to the info page
+echo "<H2>Details of most recent moon phase calculation</h2><pre>" > $infopage
+$(dirname $0)/updateMoon.sh "$now" >> $infopage
+echo "</pre>" >> $infopage
+
